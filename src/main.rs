d457257c0a35e37a1415f1c42206f777806f8981
Shaun Chong <mumk0313@gmail.com>
2022-09-29 22:03:43 +0800

    🔥 Remove old files

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,3 @@
-// use std::io;
-// use std::fs::File;
-// use std::io::prelude::*;
-
-// fn main() -> io::Result<()> {
-
-//     let mut f = File::open("src/models.rs")?;
-//     let mut buffer = [0; 10];
-
-//     let n = f.read(&mut buffer)?;
-
-//     println!("The bytes: {:?}", &buffer[..n]);
-//     Ok(())
-// }
-
-fn foo(input: Option<i32>) -> Option<i32> {
-    match input {
-        Some(i) if i >= 0 => Some(i),
-        _ => None
-    }
-}
-
 fn main() {
-    let resutl = match foo(Some(41)) {
-        Some(i) => i,
-        None => -999
-    };
-
-    println!("{}", resutl);
+    println!("개발자의블로그");
 }
