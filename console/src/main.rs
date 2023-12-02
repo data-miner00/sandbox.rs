7871bcd6e279c4079d462012ca46c9811186a207
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-02 19:58:03 +0800

    vendor: Install time and regex

--- console/src/main.rs
+++ console/src/main.rs
@@ -1,6 +1,19 @@
 use common::add;
+use time::PrimitiveDateTime as DateTime;
+use time::Duration;
+use regex::Regex;
+use time::macros::datetime;
 
 fn main() {
     println!("Hello, from console!");
     println!("Total: {}", add(6, 8));
+
+    let today = datetime!(2019-01-01 0:00);
+    let after = after(today);
+
+    println!("{}", after);
+}
+
+fn after(start: DateTime) -> DateTime {
+    start + Duration::seconds(1_000_000_000)
 }
